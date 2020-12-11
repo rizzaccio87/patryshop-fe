5d3d3056081dbbfad949ccbaf439c446803fb860
Alberto Rizzi <alberto.rizzi@siav.it>
2020-12-11 18:56:24 +0100

    Add tabs component.

--- src/app/back-office/back-office.module.ts
+++ src/app/back-office/back-office.module.ts
@@ -3,13 +3,22 @@ import {CommonModule} from '@angular/common';
 
 import {BackOfficeRoutingModule} from './back-office-routing.module';
 import {BackOfficeComponent} from './components/back-office/back-office.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {CoreModule} from '../core/core.module';
+import {SharedModule} from '../shared/shared.module';
+import {NzTabsModule} from 'ng-zorro-antd/tabs';
 
 
 @NgModule({
   declarations: [BackOfficeComponent],
   imports: [
     CommonModule,
-    BackOfficeRoutingModule
+    FormsModule,
+    BackOfficeRoutingModule,
+    ReactiveFormsModule,
+    NzTabsModule,
+    CoreModule,
+    SharedModule
   ]
 })
 export class BackOfficeModule {
