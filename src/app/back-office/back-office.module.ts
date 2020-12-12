88e98f6995eb523cc19c62d5356680150606ec52
Alberto Rizzi <alberto.rizzi@siav.it>
2020-12-12 10:56:28 +0100

    Add tab component for backoffice module.

--- src/app/back-office/back-office.module.ts
+++ src/app/back-office/back-office.module.ts
@@ -7,16 +7,21 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {CoreModule} from '../core/core.module';
 import {SharedModule} from '../shared/shared.module';
 import {NzTabsModule} from 'ng-zorro-antd/tabs';
+import {NzModalModule} from 'ng-zorro-antd/modal';
+import {BackOfficeTabComponent} from './components/back-office-tab/back-office-tab.component';
+import {NzButtonModule} from 'ng-zorro-antd/button';
 
 
 @NgModule({
-  declarations: [BackOfficeComponent],
+  declarations: [BackOfficeComponent, BackOfficeTabComponent],
   imports: [
     CommonModule,
     FormsModule,
     BackOfficeRoutingModule,
     ReactiveFormsModule,
     NzTabsModule,
+    NzButtonModule,
+    NzModalModule,
     CoreModule,
     SharedModule
   ]
--- src/app/back-office/components/back-office/back-office.component.ts
+++ src/app/back-office/components/back-office/back-office.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CakeService} from '../../../core/services/cake.service';
+import {OrderService} from '../../../core/services/order.service';
+import {Cake} from '../../../core/interfaces/cake';
+import {Observable} from 'rxjs';
+import {Order} from '../../../core/interfaces/order';
+import {GridColumn} from '../../../shared/interfaces/grid';
+import {Action} from '../../../shared/interfaces/action';
 
 @Component({
   selector: 'app-back-office',
@@ -6,10 +13,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./back-office.component.scss']
 })
 export class BackOfficeComponent implements OnInit {
+  public cakes$: Observable<Cake[]>;
+  public orders$: Observable<Order[]>;
+  public orderColumns: GridColumn[];
+  public cakeColumns: GridColumn[];
+  public orderActions: Action[];
+  public cakeActions: Action[];
 
-  constructor() { }
+  constructor(private cakeService: CakeService, private orderService: OrderService) {
+    this.orderColumns = [{
+      field: 'creationTimestamp',
+      title: 'Data'
+    }, {
+      field: 'cakeName',
+      title: 'Torta'
+    }, {
+      field: 'amount',
+      title: 'Disponibilità'
+    }, {
+      field: 'price',
+      title: 'Prezzo'
+    }];
+
+    this.cakeColumns = [{
+      field: 'name',
+      title: 'Nome Torta'
+    }, {
+      field: 'price',
+      title: 'Prezzo'
+    }];
+
+    this.orderActions = this.cakeActions = [{
+      label: 'Aggiorna',
+      type: 'update'
+    }, {
+      label: 'Rimuovi',
+      type: 'remove'
+    }];
+  }
 
   ngOnInit(): void {
+    this.cakes$ = this.cakeService.loadCakes();
+    this.orders$ = this.orderService.loadOrders();
   }
 
 }
