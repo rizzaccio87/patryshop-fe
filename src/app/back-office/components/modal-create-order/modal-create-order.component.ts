9d6362a73445e3186483e9d80facf1b4fbeb7ee2
Alberto Rizzi <alberto.rizzi@siav.it>
2020-12-12 14:27:18 +0100

    Add create order modal and skeleton for update order modal.

---
+++ src/app/back-office/components/modal-create-order/modal-create-order.component.ts
@@ -0,0 +1,49 @@
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {NzModalRef} from 'ng-zorro-antd/modal';
+import {OrderService} from '../../../core/services/order.service';
+import {Order} from '../../../core/interfaces/order';
+import {Cake} from '../../../core/interfaces/cake';
+import {CakeService} from '../../../core/services/cake.service';
+// import moment = require('moment');
+
+@Component({
+  selector: 'app-modal-create-order',
+  templateUrl: './modal-create-order.component.html',
+  styleUrls: ['./modal-create-order.component.scss']
+})
+export class ModalCreateOrderComponent {
+
+  public form: FormGroup;
+  public cakes: Cake[];
+
+  constructor(private fb: FormBuilder, private modal: NzModalRef, private orderService: OrderService, private cakeService: CakeService) {
+    this.cakeService.loadCakes().subscribe(cakes => this.cakes = cakes);
+    this.form = this.fb.group({
+      cakeId: [null, [Validators.required]],
+      amount: [0, [Validators.required]]
+    });
+  }
+
+  destroyModal(): void {
+    this.modal.destroy();
+  }
+
+  onConfirm(): void {
+    if (this.form.valid) {
+      const order = this.form.value as Order;
+      // const selectedCake = this.cakes.find(cake => cake.id === order.cakeId);
+
+      // order.cakeName = selectedCake?.name;
+      // order.creationTimestamp = moment().toDate();
+      // order.price = selectedCake.price;
+
+      this.orderService.addOrder(order).subscribe((res) => {
+        this.modal.close(res);
+      }, () => {
+        this.modal.close(false);
+      });
+    }
+  }
+
+}
---
+++ src/app/back-office/components/modal-update-order/modal-update-order.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-update-order',
+  templateUrl: './modal-update-order.component.html',
+  styleUrls: ['./modal-update-order.component.scss']
+})
+export class ModalUpdateOrderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
